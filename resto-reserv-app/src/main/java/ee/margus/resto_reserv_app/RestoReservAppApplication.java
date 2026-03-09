06d7853688abbb3f1180140984c61d8d8f3ebe02
Margus <98420540+marguspollm@users.noreply.github.com>
2026-03-09 14:05:11 +0200

    Init

---
+++ resto-reserv-app/src/main/java/ee/margus/resto_reserv_app/RestoReservAppApplication.java
@@ -0,0 +1,13 @@
+package ee.margus.resto_reserv_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoReservAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoReservAppApplication.class, args);
+	}
+
+}
