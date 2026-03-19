c115ad25802482d744669e4410ac0a4842b6f7d5
Margus <98420540+marguspollm@users.noreply.github.com>
2026-03-19 16:28:07 +0200

    Remove hard coded url

--- resto-reserv-app/src/main/java/ee/margus/resto_reserv_app/config/AppConfig.java
+++ resto-reserv-app/src/main/java/ee/margus/resto_reserv_app/config/AppConfig.java
@@ -1,6 +1,7 @@
 package ee.margus.resto_reserv_app.config;
 
 import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,14 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
 
-    // Configure CORS to allow defined frontend fetches
+    // Configure CORS to allow defined frontend methods
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "DELETE", "OPTIONS");
             }
         };
